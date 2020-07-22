7732299cf37a9679c74e9182ea40db7d8e7a4261
Kunal Kapoor <kunal.d@hotmail.co.uk>
2020-07-22 15:58:03 +0100

    enable evaluation checkking of currnet run to existing model

--- mnist/evaluate/evaluate_model.py
+++ mnist/evaluate/evaluate_model.py
@@ -111,34 +111,34 @@ try:
     model = get_latest_model(
         model_name, tag_name, exp.name, ws)
 
-    # if (model is not None):
-    #    production_model_mse = 10000
-    #    if (metric_eval in model.tags):
-    #        production_model_mse = float(model.tags[metric_eval])
-    #    new_model_mse = float(run.parent.get_metrics().get(metric_eval))
-    #    if (production_model_mse is None or new_model_mse is None):
-    #        print("Unable to find", metric_eval, "metrics, "
-    #              "exiting evaluation")
-    #        if((allow_run_cancel).lower() == 'true'):
-    #            run.parent.cancel()
-    #    else:
-    #        print(
-    #            "Current Production model mse: {}, "
-    #            "New trained model mse: {}".format(
-    #                production_model_mse, new_model_mse
-    #            )
-    #        )
-    #
-    #    if (new_model_mse < production_model_mse):
-    #        print("New trained model performs better, "
-    #              "thus it should be registered")
-    #    else:
-    #        print("New trained model metric is worse than or equal to "
-    #              "production model so skipping model registration.")
-    #        if((allow_run_cancel).lower() == 'true'):
-    #            run.parent.cancel()
-    # else:
-    print("This is the first model, ""thus it should be registered")
+    if (model is not None):
+        print(metric_eval)
+        production_model_loss = 0.6
+        if (metric_eval in model.tags):
+            production_model_loss = float(model.tags[metric_eval])
+        new_model_loss = float(run.parent.get_metrics().get(metric_eval))
+        if (production_model_loss is None or new_model_loss is None):
+            print("Unable to find", metric_eval, "metrics, "
+                  "exiting evaluation")
+            if((allow_run_cancel).lower() == 'true'):
+                run.parent.cancel()
+        else:
+            print(
+                "Current Production model mse: {}, "
+                "New trained model mse: {}".format(
+                    production_model_loss, new_model_loss
+                )
+            )
+        if (new_model_loss < production_model_loss):
+            print("New trained model performs better, "
+                  "thus it should be registered")
+        else:
+            print("New trained model metric is worse than or equal to "
+                  "production model so skipping model registration.")
+            if((allow_run_cancel).lower() == 'true'):
+                run.parent.cancel()
+    else:
+        print("This is the first model, ""thus it should be registered")
 
 except Exception:
     traceback.print_exc(limit=None, file=None, chain=True)
