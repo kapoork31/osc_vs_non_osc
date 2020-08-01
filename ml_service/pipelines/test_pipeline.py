4e52f3b5d9c5b3815278eb0cd613970b4d20d0ed
Kunal Kapoor <kunal.d@hotmail.co.uk>
2020-08-02 00:31:08 +0100

    testing pipeline

--- ml_service/pipelines/test_pipeline.py
+++ ml_service/pipelines/test_pipeline.py
@@ -1,6 +1,16 @@
+import argparse
+import sys
+import os
+from azureml.core import Run, Experiment, Workspace
+from ml_service.util.env_variables import Env
+from mnist.util.model_helper import get_latest_model
+from azureml.pipeline.core import PublishedPipeline
+
 def main():
     print('hello')
 
+    run = Run.get_context()
+    print(run)
 
 if __name__ == '__main__':
     main()
